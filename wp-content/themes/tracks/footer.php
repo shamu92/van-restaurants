792869e9b97fd9457d8c355cda71e66dd90ef561
shamu92 <altshteyn@gmail.com>
2017-05-09 10:18:59 -0700

    footer font

--- wp-content/themes/tracks/footer.php
+++ wp-content/themes/tracks/footer.php
@@ -17,7 +17,7 @@
 	get_template_part( 'sidebar', 'footer' );
 	?>
 	<div class="design-credit">
-		<marquee behaviour=”scroll” direction=”left” SCROLLDELAY=2 SCROLLAMOUNT=4 MAX-WIDTH 1340 HEIGHT=30 style=”color:#00cc66”><p>Where do you love to eat?</p></marquee>
+		<marquee behaviour=”scroll” direction=”left” SCROLLDELAY=2 SCROLLAMOUNT=4 MAX-WIDTH 1340 HEIGHT=30 style=”color:#00cc66”><h4>Where do you love to eat?</h4></marquee>
 
 
 
